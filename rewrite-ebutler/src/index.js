ade5a633ee494fc1dabadb35fafbdebba1f7f3b5
Leslie Mahasha <LMahasha@jhb.dvt.co.za>
2017-11-22 14:44:41 +0200

    added the three sections of the view

--- rewrite-ebutler/src/index.js
+++ rewrite-ebutler/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ebutler from './js/components/ebutler';
-import nav from './js/components/navigation';
-import slider from './js/components/slider';
+import Ebutler from './js/components/ebutler';
 import registerServiceWorker from './js/components/registerServiceWorker';
 
-ReactDOM.render(<ebutler />, document.getElementById('ebutler'));
-ReactDOM.render(<slider />, document.getElementsByClassName('container-fluid'));
-ReactDOM.render(<nav />, document.getElementsByClassName('navbar'));
+ReactDOM.render(<Ebutler />, document.body);
 
 registerServiceWorker();
--- rewrite-ebutler/src/js/components/ebutler.js
+++ rewrite-ebutler/src/js/components/ebutler.js
@@ -1,21 +1,112 @@
-import React, { Component } from 'react';
-/*import '../index.css';*/
+import React from 'react';
+import '../../index.css';
 
-  class ebutler extends Component {
+/*import menu from './navigation';
+import slider from './slider';*/
+  class Ebutler extends React.Component {
     render() {
       return (
-        <div className="ebutler">
-          <header className="App-header">
-            
+        <div id="ebutler">
+          <header id="navigation">
+            <Navigation />
           </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+
+          <section id="slider">
+            {/*<Slider />*/}
+          </section>
+
+          <section id="products">
+              <article></article>
+
+              <article></article>
+
+              <article></article>
+          </section>
+
+          <section id="footerSlider"></section>
+
+          <footer id="footer"></footer>
         </div>
-      );
+      )
     }
 
   }
 
+  class Navigation extends React.Component{
+    
+    render(){
+        return(
+            <nav className="navbar navbar-expand-lg navbar-dark">
+                <a className="navbar-brand logo" href="/">&nbsp;</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                            Shop
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="/">Shop</a>
+                                <a className="dropdown-item" href="/">aaaaa</a>
+                            </div>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                            Promotions
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="/">Shop</a>
+                                <a className="dropdown-item" href="/">bbbbb</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <input type="search" name="search" className="col-lg-6" placeholder="Search" />
 
-export default ebutler;
+                    <a href="/" className="btn btn-sm btn-rounded btn-top">Help</a>
+                    <a href="/" className="btn btn-sm btn-rounded btn-top">Get Started</a>
+                    <a href="/"><img className="img-responsive" src="../images/assets/cart.png" alt="Cart"/></a>
+                </div>                    
+            </nav>
+        );
+    }
+}
+
+class Slider extends React.Component{
+  render(){
+          return(
+              <div className="row">        
+                  <div id="carousel-example-1z" className="carousel slide carousel-fade" data-ride="carousel">
+                      
+                      <ol className="carousel-indicators">
+                          <li data-target="#carousel-example-1z" data-slide-to="0" className="active"></li>
+                          <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+                          <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+                      </ol>
+                      
+                      <div className="carousel-inner" role="listbox">
+
+                          <div className="carousel-item active">
+                              <img className="d-block w-100" src="../images/assets/top-banner1.png" alt="First slide"/>
+                          </div>
+                          
+                          <div className="carousel-item">
+                              <img className="d-block w-100" src="../images/assets/top-banner2.png" alt="Second slide"/>
+                          </div>
+                         
+                          <div className="carousel-item">
+                              <img className="d-block w-100" src="../images/assets/top-banner3.png" alt="Third slide"/>
+                          </div>
+                          
+                      </div>
+                  </div>
+              </div>
+          );
+  }
+}
+
+export default Ebutler;
